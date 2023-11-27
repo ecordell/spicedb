79af857b76e258ebac53c151ad2acff116c69eea
Evan Cordell <cordell.evan@gmail.com>
2023-11-27 10:51:28 -0500

    wip memdb

--- internal/datastore/memdb/memdb_test.go
+++ internal/datastore/memdb/memdb_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"strconv"
 	"sync/atomic"
 	"testing"
 	"time"
@@ -13,8 +14,9 @@ import (
 
 	"github.com/authzed/spicedb/pkg/datastore"
 	"github.com/authzed/spicedb/pkg/datastore/options"
-	test "github.com/authzed/spicedb/pkg/datastore/test"
+	"github.com/authzed/spicedb/pkg/datastore/test"
 	ns "github.com/authzed/spicedb/pkg/namespace"
+	nsutil "github.com/authzed/spicedb/pkg/namespace"
 	corev1 "github.com/authzed/spicedb/pkg/proto/core/v1"
 	"github.com/authzed/spicedb/pkg/tuple"
 )
@@ -113,3 +115,128 @@ func TestConcurrentWriteRelsError(t *testing.T) {
 	require.Error(werr)
 	require.ErrorContains(werr, "serialization max retries exceeded")
 }
+
+func TestOptimizedRevisionSnapshotEvaluation(t *testing.T) {
+	require := require.New(t)
+
+	ds, err := NewMemdbDatastore(0, 1*time.Second, 1*time.Hour)
+	require.NoError(err)
+
+	ctx := context.Background()
+
+	setupDatastore(ds, require)
+
+	head, err := ds.HeadRevision(ctx)
+	require.NoError(err)
+
+	// no tuples at head revision, nothing written yet
+	tuple0 := readExactTuple(ds.SnapshotReader(head), makeTestTuple(0, 0), require)
+	require.Equal("", tuple0)
+
+	// write a tuple
+	writeRev, err := ds.ReadWriteTx(ctx, func(ctx context.Context, rwt datastore.ReadWriteTransaction) error {
+		return rwt.WriteRelationships(ctx, []*corev1.RelationTupleUpdate{{
+			Operation: corev1.RelationTupleUpdate_TOUCH,
+			Tuple:     makeTestTuple(0, 0),
+		}})
+	})
+	require.NoError(err)
+
+	// write another tuple
+	writeRev2, err := ds.ReadWriteTx(ctx, func(ctx context.Context, rwt datastore.ReadWriteTransaction) error {
+		return rwt.WriteRelationships(ctx, []*corev1.RelationTupleUpdate{{
+			Operation: corev1.RelationTupleUpdate_TOUCH,
+			Tuple:     makeTestTuple(1, 1),
+		}})
+	})
+	require.NoError(err)
+
+	optimizedRev, err := ds.OptimizedRevision(ctx)
+	require.NoError(err)
+	require.True(optimizedRev.LessThan(writeRev))
+	require.True(optimizedRev.LessThan(writeRev2))
+
+	// tuple should be visible at the revision returned by the write
+	tuple1AtWriteRev := readExactTuple(ds.SnapshotReader(writeRev2), makeTestTuple(1, 1), require)
+	require.Equal(makeTestTuple(1, 1).String(), tuple1AtWriteRev)
+
+	// tuple should not be visible at optimized revision, which is before the write revision
+	tuple1AtOptimizedRev := readExactTuple(ds.SnapshotReader(optimizedRev), makeTestTuple(1, 1), require)
+	require.Equal("", tuple1AtOptimizedRev)
+}
+
+var testResourceNS = nsutil.Namespace(
+	testResourceNamespace,
+	nsutil.MustRelation(testReaderRelation, nil),
+)
+
+var testGroupNS = nsutil.Namespace(
+	testGroupNamespace,
+	nsutil.MustRelation(testMemberRelation, nil),
+)
+
+var testUserNS = nsutil.Namespace(testUserNamespace)
+
+func readExactTuple(reader datastore.Reader, tuple *corev1.RelationTuple, require *require.Assertions) (out string) {
+	itr, err := reader.QueryRelationships(context.Background(), tupleToFilter(tuple))
+	require.NoError(err)
+	defer itr.Close()
+
+	for tpl := itr.Next(); tpl != nil; tpl = itr.Next() {
+		if len(out) > 0 {
+			require.Fail("found multiple matching exact tuples; test error")
+		}
+		out = tuple.String()
+	}
+	return
+}
+
+func tupleToFilter(tuple *corev1.RelationTuple) datastore.RelationshipsFilter {
+	return datastore.RelationshipsFilter{
+		ResourceType:             testResourceNamespace,
+		OptionalResourceIds:      []string{tuple.ResourceAndRelation.ObjectId},
+		OptionalResourceRelation: tuple.ResourceAndRelation.Relation,
+		OptionalSubjectsSelectors: []datastore.SubjectsSelector{{
+			OptionalSubjectType: tuple.Subject.Namespace,
+			OptionalSubjectIds:  []string{tuple.Subject.ObjectId},
+			RelationFilter: datastore.SubjectRelationFilter{
+				IncludeEllipsisRelation: true,
+			},
+		}},
+	}
+}
+
+func makeTestTuple(resourceID, userID int) *corev1.RelationTuple {
+	return &corev1.RelationTuple{
+		ResourceAndRelation: &corev1.ObjectAndRelation{
+			Namespace: testResourceNamespace,
+			ObjectId:  strconv.Itoa(resourceID),
+			Relation:  testReaderRelation,
+		},
+		Subject: &corev1.ObjectAndRelation{
+			Namespace: testUserNamespace,
+			ObjectId:  strconv.Itoa(userID),
+			Relation:  ellipsis,
+		},
+	}
+}
+
+func setupDatastore(ds datastore.Datastore, require *require.Assertions) datastore.Revision {
+	ctx := context.Background()
+
+	revision, err := ds.ReadWriteTx(ctx, func(ctx context.Context, rwt datastore.ReadWriteTransaction) error {
+		return rwt.WriteNamespaces(ctx, testGroupNS, testResourceNS, testUserNS)
+	})
+	require.NoError(err)
+
+	return revision
+}
+
+const (
+	testUserNamespace     = "test/user"
+	testResourceNamespace = "test/resource"
+	testGroupNamespace    = "test/group"
+	testReaderRelation    = "reader"
+	testMemberRelation    = "member"
+	ellipsis              = "..."
+)
--- internal/datastore/memdb/revisions_test.go
+++ internal/datastore/memdb/revisions_test.go
@@ -24,6 +24,30 @@ func TestHeadRevision(t *testing.T) {
 	require.NoError(t, err)
 	err = ds.CheckRevision(context.Background(), newer)
 	require.NoError(t, err)
+
+	require.Equal(t, older, newer)
+}
+
+func TestOptimizedRevision(t *testing.T) {
+	quantPeriod := 500 * time.Millisecond
+
+	ds, err := NewMemdbDatastore(0, quantPeriod, 1*time.Hour)
+	require.NoError(t, err)
+
+	head, err := ds.HeadRevision(context.Background())
+	require.NoError(t, err)
+
+	rev1, err := ds.OptimizedRevision(context.Background())
+	require.NoError(t, err)
+
+	require.True(t, rev1.LessThan(head))
+
+	time.Sleep(quantPeriod)
+	rev2, err := ds.OptimizedRevision(context.Background())
+	require.NoError(t, err)
+
+	require.True(t, rev1.LessThan(rev2))
+	require.True(t, head.LessThan(rev2))
 }
 
 func (mdb *memdbDatastore) ExampleRetryableError() error {
